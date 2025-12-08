979823cc0b60e03e1c3257bc9c3985b2167eb37c
Yaminikanta Prusty <yaminikanta143@gmail.com>
2025-12-08 22:18:09 +0530

    first commit of this series

---
+++ 01_basics/chai.py
@@ -0,0 +1,29 @@
+from hello_chai import chai
+
+chai('ginger tea')
+
+
+
+"""
+first we create hello_chai.py then we create chai.py 
+
+in this we import the hello_chia file after import when we run it, it run but the __pycache__ file got created autometicaly 
+
+"""
+
+
+"""
+when we work in shell means terminal if we impoert file then did some modification then the modified code will not import 
+bcz it was written after import so to overcome that we need to import reload module thrn reload the file
+
+
+import hello_chai
+
+from importlib import reload             (imoport reload module)
+
+reload(hello_chai)                       (reload the file)
+
+hello_chai.chai_one                      (then it will work)           (refer youtube vdo no 4)
+
+
+"""
---
+++ 01_basics/hello_chai.py
@@ -0,0 +1,12 @@
+print("chai aur python")
+
+
+def chai(n):
+    print(n)
+
+chai(4)
+chai("lemon tea")
+
+chai_one = 'chai one'
+chai_two = 'chai_two'
+chai_three = 'chai_three'
